a549e21eb54630cf8bbb66e22492fca7f7098306
l-h-e <imperialbanner_1025@yahoo.co.jp>
2019-10-19 23:41:30 +0900

    init

---
+++ Assets/buttonScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonScript : MonoBehaviour
+{
+
+    bool flag = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flag==true)
+        { // Editor/マウス操作の場合は Input.GetMouseButton(0) にする  Input.touchCount > 0 &&
+           // Debug.Log("aaaa");
+            var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.z = 0;
+            var poss = new Vector3(pos.x, this.transform.position.y, pos.z);
+            this.transform.position = poss;
+        }
+    }
+
+    public void OnUp()
+    {
+        flag = false;
+    }
+
+    public void OnDown()
+    {
+       // Debug.Log("teusi");
+        flag = true;
+    }
+}
---
+++ Assets/circleCon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class circleCon : MonoBehaviour
+{
+    GameObject p;
+    // Start is called before the first frame update
+    void Start()
+    {
+        p = GameObject.Find("Square");
+        this.transform.parent = p.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("pos:"+this.transform.position);
+    }
+}
